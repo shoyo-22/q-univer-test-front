bb972cbe3c97c34702cccb0e77523e0164a82e7d
shoyo <shoyo2209@gmail.com>
2025-04-01 23:18:26 +0500

    refactor: draft menu toggle button

--- src/app/layouts/GuestLayout.vue
+++ src/app/layouts/GuestLayout.vue
@@ -1,10 +1,10 @@
 <template>
   <header class="py-3 sm:py-4">
     <BaseContainer class="flex items-center justify-between">
-      <h1 class="flex-1 text-lg font-bold transition-all duration-300 sm:text-xl lg:text-2xl">
+      <h1 class="text-lg font-bold transition-all duration-300 sm:text-xl lg:text-2xl">
         Q University
       </h1>
-      <div class="">
+      <div class="max-sm:hidden">
         <button
           class="flex cursor-pointer items-center gap-1.5 rounded-lg border border-gray-300 px-3 py-1.5 text-xs font-medium text-gray-600 shadow-xs transition-all duration-300 hover:border-blue-400 hover:bg-blue-50 hover:text-blue-800 sm:px-4 sm:py-2 sm:text-sm"
         >
@@ -12,6 +12,41 @@
           <ArrowRightEndOnRectangleIcon class="size-4 sm:size-5" />
         </button>
       </div>
+      <button
+        class="group inline-flex h-6 w-6 items-center justify-center rounded bg-white text-center text-gray-800 transition sm:hidden"
+        aria-pressed="false"
+        onclick="this.setAttribute('aria-pressed', !(this.getAttribute('aria-pressed') === 'true'))"
+        @click="toggleMenu"
+      >
+        <span class="sr-only">Menu</span>
+        <svg
+          class="pointer-events-none h-6 w-6 fill-current"
+          viewBox="0 0 16 16"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            class="origin-center translate-x-[7px] -translate-y-[5px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-[[aria-pressed=true]]:translate-x-0 group-[[aria-pressed=true]]:translate-y-0 group-[[aria-pressed=true]]:rotate-[315deg]"
+            y="7"
+            width="9"
+            height="2"
+            rx="1"
+          ></rect>
+          <rect
+            class="origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)] group-[[aria-pressed=true]]:rotate-45"
+            y="7"
+            width="16"
+            height="2"
+            rx="1"
+          ></rect>
+          <rect
+            class="origin-center translate-y-[5px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-[[aria-pressed=true]]:translate-y-0 group-[[aria-pressed=true]]:-rotate-[225deg]"
+            y="7"
+            width="9"
+            height="2"
+            rx="1"
+          ></rect>
+        </svg>
+      </button>
     </BaseContainer>
   </header>
   <RouterView />
@@ -20,5 +55,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import { BaseContainer } from '@/shared/ui';
+import { ref } from 'vue';
 import { ArrowRightEndOnRectangleIcon } from '@heroicons/vue/24/solid';
+
+const isActive = ref<boolean>(false);
+
+const toggleMenu = () => {
+  isActive.value = !isActive.value;
+};
 </script>
